9e334228e1a76a57e57719f4f838ce7a0433025a
laco0416 <laco0416@gmail.com>
2012-12-20 14:28:07 +0900

    レポジトリfix

--- src/net/miz_hi/warotter/view/MainActivity.java
+++ src/net/miz_hi/warotter/view/MainActivity.java
@@ -3,33 +3,28 @@ package net.miz_hi.warotter.view;
 import com.slidingmenu.lib.SlidingMenu;
 
 import net.miz_hi.warotter.R;
-import net.miz_hi.warotter.WarotterApplication;
 import net.miz_hi.warotter.core.EventBindingActivity;
 import net.miz_hi.warotter.core.ViewModel;
 import net.miz_hi.warotter.util.EnumRequestCode;
 import net.miz_hi.warotter.viewmodel.MainActivityViewModel;
-import gueei.binding.Command;
-import gueei.binding.labs.EventSubscriber;
-import gueei.binding.observables.StringObservable;
-import android.app.Application;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends EventBindingActivity
 {
 
 	
+	@Override
 	public void onCreate(Bundle bundle)
 	{
 		super.onCreate(bundle);
 		this.setAndBindRootView(R.layout.mainactivity_layout, viewModel);
 		SlidingMenu menu = new SlidingMenu(this);
 		menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-        menu.setShadowWidthRes(R.dimen.shadow_width);
-        menu.setShadowDrawable(R.drawable.shadow);
-        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-        menu.setFadeDegree(0.35f);
-        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);        
+		menu.setShadowWidthRes(R.dimen.shadow_width);
+		menu.setShadowDrawable(R.drawable.shadow);
+		menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		menu.setFadeDegree(0.35f);
+		menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
 	}
 
 	@Override
