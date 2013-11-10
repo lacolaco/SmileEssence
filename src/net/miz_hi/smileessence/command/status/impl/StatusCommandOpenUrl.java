5b7b73551a7815251d54e2a16ee0a749ee2d6856
laco0416 <laco0416@gmail.com>
2013-11-10 12:17:17 +0900

    added StatusCommandOpenUrl: open status in browser

---
+++ src/net/miz_hi/smileessence/command/status/impl/StatusCommandOpenUrl.java
@@ -0,0 +1,30 @@
+package net.miz_hi.smileessence.command.status.impl;
+
+import android.app.Activity;
+import net.miz_hi.smileessence.command.CommandOpenUrl;
+import net.miz_hi.smileessence.command.status.StatusCommand;
+import net.miz_hi.smileessence.model.status.tweet.TweetModel;
+
+public class StatusCommandOpenUrl extends StatusCommand
+{
+
+    private Activity activity;
+
+    public StatusCommandOpenUrl(TweetModel status, Activity activity)
+    {
+        super(status);
+        this.activity = activity;
+    }
+
+    @Override
+    public void workOnUiThread()
+    {
+        new CommandOpenUrl(activity, String.format("https://twitter.com/%s/status/%s", status.user.screenName, status.statusId)).run();
+    }
+
+    @Override
+    public String getName()
+    {
+        return "ブラウザで開く";
+    }
+}
