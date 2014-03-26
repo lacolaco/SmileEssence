6c585ec71d48172cce0a940b6f7c0eb2719feba2
laco0416 <laco0416@gmail.com>
2014-03-26 12:51:01 +0900

    Refactor MainActivity.java

--- src/net/lacolaco/smileessence/activity/MainActivity.java
+++ src/net/lacolaco/smileessence/activity/MainActivity.java
@@ -29,7 +29,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -214,7 +213,7 @@ public class MainActivity extends Activity
         return appPref.getValue("app.version", "");
     }
 
-    public boolean isFirstLaunchThisVersion()
+    private boolean isFirstLaunchThisVersion()
     {
         return !getVersion().contentEquals(getLastLaunchVersion());
     }
@@ -246,19 +245,19 @@ public class MainActivity extends Activity
         return viewPager;
     }
 
-    public PagerAdapter getPagerAdapter()
+    public PageListAdapter getPagerAdapter()
     {
         return pagerAdapter;
-    }
-
-    public void setCurrentAccount(Account currentAccount)
-    {
-        this.currentAccount = currentAccount;
     }
 
     public Account getCurrentAccount()
     {
         return currentAccount;
+    }
+
+    public void setCurrentAccount(Account currentAccount)
+    {
+        this.currentAccount = currentAccount;
     }
 
     /**
