91080f77a4b70e0c7c1f0a9620f38947f1379787
laco0416 <laco0416@gmail.com>
2014-03-28 10:49:44 +0900

    Add SettingActivity and SettingFragment (not yet implemented)

---
+++ SmileEssenceTest/src/net/lacolaco/smileessence/activity/SettingActivityTest.java
@@ -0,0 +1,47 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2012-2014 lacolaco.net
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.lacolaco.smileessence.activity;
+
+import android.app.Fragment;
+import android.test.ActivityInstrumentationTestCase2;
+import net.lacolaco.smileessence.R;
+import net.lacolaco.smileessence.view.SettingFragment;
+
+public class SettingActivityTest extends ActivityInstrumentationTestCase2<SettingActivity>
+{
+
+    public SettingActivityTest()
+    {
+        super(SettingActivity.class);
+    }
+
+    public void testGetValues() throws Exception
+    {
+        Fragment fragment = getActivity().getFragmentManager().findFragmentById(R.id.fragment_setting);
+        assertNotNull(fragment);
+        assertTrue(fragment instanceof SettingFragment);
+
+    }
+}
